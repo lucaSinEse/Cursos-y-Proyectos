0f1c3c63f5b0c3b3ddb550cf23a42a9a1cf3cf3e
Luca Cussino <lucacussino@gmail.com>
2023-12-30 18:49:55 -0300

    params

--- Django/myapp/views.py
+++ Django/myapp/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
 # Create your views here.
-def hello(request):
-    return HttpResponse("<h1>Hello World</h1>")
+def hello(request, username):
+    return HttpResponse(f"<h1>Hello {username}</h1>")
 
 def about(request):
     return HttpResponse('About')
 
+def index(request):
+    return HttpResponse("<h1>Index Page</h1>")
