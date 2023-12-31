d6129d394c1f2cc40cb4e347cf056bb72300f8e6
Luca Cussino <lucacussino@gmail.com>
2023-12-31 15:05:33 -0300

    params y models

--- Django/myapp/views.py
+++ Django/myapp/views.py
@@ -1,4 +1,6 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from .models import Project, Task
+from django.shortcuts import get_object_or_404
 # Create your views here.
 def hello(request, username):
     return HttpResponse(f"<h1>Hello {username}</h1>")
@@ -8,3 +10,11 @@ def about(request):
 
 def index(request):
     return HttpResponse("<h1>Index Page</h1>")
+
+def projects(request):
+    projects = list(Project.objects.values())
+    return JsonResponse(projects, safe=False)
+
+def tasks(request, title):
+    tasks = get_object_or_404(Task, id=id)
+    return HttpResponse(f'task: {tasks.title}')
