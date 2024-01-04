48abc638edaac0a97e15a94c74aa71ea719523db
Luca Cussino <lucacussino@gmail.com>
2024-01-04 12:04:18 -0300

    estilos (mitad)

---
+++ Reflex/link_bio/link_bio/styles/styles.py
@@ -0,0 +1,42 @@
+from enum import Enum
+import reflex as rx
+#Constants
+MAX_WIDTH = "600px"
+
+#Sizes
+
+class Size(Enum):
+    SMALL = "0.5em"
+    MEDIUM = "0.8em"
+    DEFAULT = "1em"
+    BIG = "2em"
+
+#Styles
+
+BASE_STYLE = {
+    rx.Button:{
+        "width": "100%",
+        "height": "100%",
+        "display": "block",
+        "padding": Size.SMALL.value,
+        "border_radius": Size.DEFAULT.value,
+    },
+    rx.Link:{
+        "text_decoration" : "none",
+        "_hover":{}
+
+    }
+}
+title_style = dict(
+    size = "md",
+    width = "100%",
+    padding_top = Size.DEFAULT.value
+)
+
+button_title_style = dict(
+    font_size = Size.DEFAULT.value
+)
+button_body_style = dict(
+    font_size = Size.MEDIUM.value
+)
+
--- Reflex/link_bio/link_bio/views/links/links.py
+++ Reflex/link_bio/link_bio/views/links/links.py
@@ -1,10 +1,13 @@
 import reflex as rx
 from link_bio.components.link_button import link_button
+from link_bio.components.title import title
 
 def links() ->rx.Component:
     return rx.vstack(
-        link_button("Twitch","https://www.twitch.tv/"),
-        link_button("Youtube", "https://www.youtube.com/"),
-        link_button("Linkedin", "https://www.linkedin.com/"),
-        link_button("Netflix", "https://www.netflix.com/"),
+        title("Titulo que me de la gana"),
+        link_button("Twitch","directos","https://www.twitch.tv/"),
+        link_button("Youtube","videos", "https://www.youtube.com/"),
+        link_button("Linkedin","laburo", "https://www.linkedin.com/"),
+        link_button("Netflix","entretenimiento", "https://www.netflix.com/"),
+        width = "100%"
     )
