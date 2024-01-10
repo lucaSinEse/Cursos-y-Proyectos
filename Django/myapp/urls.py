d5fb3f9bfa80d231579151a02d6d817f046bebaf
Luca Cussino <lucacussino@gmail.com>
2024-01-10 17:08:26 -0300

    formularios

--- Django/myapp/urls.py
+++ Django/myapp/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     
     path('projects/', views.projects),
     path('tasks/', views.tasks),
+    path('create_task/', views.create_task),
 ]
--- Django/myapp/views.py
+++ Django/myapp/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse, JsonResponse
 from .models import Project, Task
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
+from .forms import CreateNewTask
 # Create your views here.
 def hello(request, username):
     return HttpResponse(f"hola, {username}")
@@ -31,3 +32,16 @@ def tasks(request):
     return render(request, 'tasks.html',{
         'tasks': tasks
     })
+
+def create_task(request):
+    if request.method == 'GET':
+        #Show interface
+        return render(request, 'create_task.html',{
+            'form': CreateNewTask()})
+    else:
+        Task.objects.create(
+            title = request.POST['title'], 
+            description = request.POST['description'], 
+            project = Project.objects.get(id=2)
+        )
+        return redirect("/tasks/")
