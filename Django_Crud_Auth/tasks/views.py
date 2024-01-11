df325a2c31f95ffe9725a811b83d1530d3ec1817
Luca Cussino <lucacussino@gmail.com>
2024-01-11 16:54:18 -0300

    formulario Signup

--- Django_Crud_Auth/tasks/views.py
+++ Django_Crud_Auth/tasks/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 
 def hello_world(request):
-    return HttpResponse('Hello World')
+    return render(request, "signup.html",{
+        'form': UserCreationForm
+    })
