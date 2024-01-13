915640b5ffc9dc0ae92a0f12ba5f6fcde3529852
Luca Cussino <lucacussino@gmail.com>
2024-01-13 20:33:58 -0300

    obtener tarea

--- Django_Crud_Auth/tasks/views.py
+++ Django_Crud_Auth/tasks/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.contrib.auth import login, logout, authenticate
@@ -89,3 +89,9 @@ def create_task(request):
                 'form': TaskForm,
                 'error': 'Hay algo mal que no esta bien'
             })
+
+def task_detail(request, task_id):
+    task = get_object_or_404(Task, pk = task_id)
+    return render(request, 'task_detail.html',{
+        'task':task
+    })
