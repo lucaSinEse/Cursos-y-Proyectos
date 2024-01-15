9f5734931722bc40fcf459f443600da6f67e7295
Luca Cussino <lucacussino@gmail.com>
2024-01-15 13:12:23 -0300

    completar y eiminar tareas

--- Django_Crud_Auth/tasks/views.py
+++ Django_Crud_Auth/tasks/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth import login, logout, authenticate
 from django.db import IntegrityError
 from .forms import TaskForm
 from .models import Task
+from django.utils import timezone
 
 # Create your views here.
 
@@ -110,3 +111,16 @@ def task_detail(request, task_id):
                 'form': form,
                 'error': 'error actualizando updating task'
             })
+
+def complete_task(request, task_id):
+    task = get_object_or_404(Task, pk=task_id, user = request.user)
+    if request.method == 'POST':
+        task.date_completed = timezone.now()
+        task.save()
+        return redirect('tasks')
+
+def delete_task(request, task_id):
+    task = get_object_or_404(Task, pk=task_id, user = request.user)
+    if request.method == 'POST':
+        task.delete()
+        return redirect('tasks')
