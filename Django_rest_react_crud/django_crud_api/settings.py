7f10d020ba28fae4e1642faef535f95557c499a2
Luca Cussino <lucacussino@gmail.com>
2024-01-17 19:02:29 -0300

    docs

--- Django_rest_react_crud/django_crud_api/settings.py
+++ Django_rest_react_crud/django_crud_api/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'coreapi',
     'tasks',
     'rest_framework',
     'corsheaders',
@@ -127,5 +128,9 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
-    
+
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS' : 'rest_framework.schemas.coreapi.AutoSchema'
+}
