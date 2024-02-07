085688997e13f63d8900119d27c8e19ac9bb5000
Luca Cussino <lucacussino@gmail.com>
2024-02-07 14:40:39 -0300

    principio de testing

---
+++ Selenium/hello_world.py
@@ -0,0 +1,24 @@
+import unittest
+from pyunitreport import HTMLTestRunner
+from selenium import webdriver
+
+class HelloWorld(unittest.TestCase):
+    @classmethod
+    def setUp(cls):
+        cls.driver = webdriver.Chrome()
+        driver = cls.driver
+        driver.implicitly_wait(10)
+    
+    def test_hello_world(self):
+        driver = self.driver
+        driver.get('https://www.google.com')
+
+    def test_visit_wikipedia(self):
+        driver = self.driver
+        driver.get('https://www.wikipedia.org')
+
+    def tearDown(cls):
+        cls.driver.quit()
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2, testRunner=HTMLTestRunner(output='reportes', report_name= 'hello-world-report'))
