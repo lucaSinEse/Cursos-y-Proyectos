896669580e513c2e713bcb47bd66782018351434
lucaSinEse <lucacussino@gmail.com>
2024-03-15 08:59:39 -0300

    creacion de modelos con SQLAlchemy

--- Fast_API_Platzi/main.py
+++ Fast_API_Platzi/main.py
@@ -5,10 +5,14 @@ from pydantic import BaseModel, Field
 from typing import Coroutine, Optional, List
 from jwt_manager import create_token, validate_token
 from fastapi.security import HTTPBearer
+from config.database import Session, engine, Base
+from models.movie import Movie
 
 app = FastAPI()
 app.title = 'Mi aplicacion con FastAPI'
 app.version = "0.0.1"
+
+Base.metadata.create_all(bind=engine)
 
 class JWTBearer(HTTPBearer):
     async def __call__(self, request: Request):
