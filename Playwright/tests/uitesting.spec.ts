b8091b26742145a966fde513facc352386ef507d
lucaSinEse <lucacussino@gmail.com>
2024-05-17 12:12:54 -0300

    comienzo de playwright

---
+++ Playwright/tests/uitesting.spec.ts
@@ -0,0 +1,9 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+    await page.goto('http://uitestingplayground.com/');
+    await page.getByRole('link', { name: 'Resources' }).click();
+    await page.getByRole('link', { name: 'Home' }).click();
+    await page.getByRole('link', { name: 'Click' }).click();
+    await page.getByRole('button', { name: 'Button That Ignores DOM Click' }).click();
+});
