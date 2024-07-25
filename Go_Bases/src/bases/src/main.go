765e1d551a975aa13d8cf45953786816a23779b4
lucaSinEse <lucacussino@gmail.com>
2024-07-25 19:04:36 -0300

    COncurrencia

--- Go_Bases/src/bases/src/main.go
+++ Go_Bases/src/bases/src/main.go
@@ -1,15 +1,37 @@
 package main
 
-import (
-	"fmt"
-	pk "go_bases/src/bases/src/mypackage"
-)
+import "fmt"
 
+func message(text string, c chan string) {
+	c <- text
+}
 func main() {
-	var myCar pk.CarPublic
-	myCar.Brand = "Ferrari"
-	myCar.Year = 2020
-	fmt.Println(myCar)
+	c := make(chan string, 2)
+	c <- "Mensaje 1"
+	c <- "Mensaje 2"
+	fmt.Println(len(c), cap(c))
 
-	pk.PrintMessage("holaaaa")
+	//Close
+	close(c)
+	//c <- "Mensaje 3"
+
+	//Recorrido (range)
+	for message := range c {
+		fmt.Println(message)
+	}
+
+	//Select
+	email1 := make(chan string)
+	email2 := make(chan string)
+
+	go message("mensaje 1", email1)
+	go message("mensaje 2", email2)
+	for i := 0; i < 2; i++ {
+		select {
+		case m1 := <-email1:
+			fmt.Println("Email recibido de email1:", m1)
+		case m2 := <-email2:
+			fmt.Println("Email recibido de email2:", m2)
+		}
+	}
 }
