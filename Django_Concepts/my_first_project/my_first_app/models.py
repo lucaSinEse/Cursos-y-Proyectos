ee034991df74841d320b6b7f2f945c5b449985b7
lucaSinEse <lucacussino@gmail.com>
2024-08-16 15:01:32 -0300

    Relaciones de 1 a muchos

--- Django_Concepts/my_first_project/my_first_app/models.py
+++ Django_Concepts/my_first_project/my_first_app/models.py
@@ -2,5 +2,25 @@ from django.db import models
 
 # Create your models here.
 
-class car(models.Model):
+class Car(models.Model):
   title = models.TextField(max_length=250)
+  year = models.TextField(max_length=4, null=True)
+  color = models.TextField(max_length=20, default="White")
+
+  def __str__(self) -> str:
+    return f"{self.title} - {self.year}"
+  
+class Publisher (models.Model):
+  name = models.TextField(max_length= 100)
+  address = models.TextField(max_length= 100)
+
+  def __str__(self) -> str:
+    return f"{self.name} - {self.address}"
+  
+class Book (models.Model):
+  title = models.TextField(max_length=100)
+  publication_date = models.DateField()
+  publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
+
+  def __str__(self) -> str:
+    return f"{self.title} - {self.publication_date}"
