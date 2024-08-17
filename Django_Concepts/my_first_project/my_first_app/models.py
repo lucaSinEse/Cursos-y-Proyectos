bdc400cdadb4cf334fd87317b59029550f0c1a72
lucaSinEse <lucacussino@gmail.com>
2024-08-17 17:08:22 -0300

    rutas

--- Django_Concepts/my_first_project/my_first_app/models.py
+++ Django_Concepts/my_first_project/my_first_app/models.py
@@ -17,10 +17,22 @@ class Publisher (models.Model):
   def __str__(self) -> str:
     return f"{self.name} - {self.address}"
   
+class Author (models.Model):
+  name = models.TextField(max_length=200, default=" ")
+  birth_date = models.DateField()
+
+  def __str__(self) -> str:
+    return f"{self.name} - {self.birth_date}"
+
+class Profile(models.Model):
+  website = models.URLField()
+  biography = models.TextField(max_length=500)
+  author = models.OneToOneField(Author, on_delete=models.CASCADE)
 class Book (models.Model):
   title = models.TextField(max_length=100)
   publication_date = models.DateField()
   publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
+  authors = models.ManyToManyField(Author, related_name="authors")
 
   def __str__(self) -> str:
     return f"{self.title} - {self.publication_date}"
